4c0fc4f5c4b385ea1199020b15d097c2e6ea8f79
vkv2023 <vashistha.vinod@gmail.com>
2024-06-22 00:45:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# django-saml-service-provider-master
+ Easily let users sign in via SAML 2.0 to your django app. Based on python-saml and comes with a Onelogin.com provider, so you need to do very little work to get started.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from saml_service_provider import __version__ as version
+
+setup(
+    name='django-saml-service-provider',
+    version=version,
+    description='Easily let users sign in via SAML 2.0 to your django app.',
+    long_description='',
+    author='',
+    author_email='vashisthavinod@yahoo.com',
+    url='',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.4',
+        'python-saml'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
